db68950dde0a8f5c7f1dbda71b2851e0bd2027c7
soeyusuke <s1240231@gmail.com>
2018-06-20 18:17:12 +0900

    [ADD] init mode

---
+++ mode.go
@@ -0,0 +1,25 @@
+package main
+
+type Mode int
+
+const (
+	Normal Mode = iota
+	Insert
+	Visual
+)
+
+var (
+	mode Mode
+)
+
+func (m Mode) String() string {
+	switch m {
+	case Normal:
+		return "NORMAL"
+	case Insert:
+		return "INSERT"
+	case Visual:
+		return "VISUAL"
+	}
+	return "non mode"
+}
